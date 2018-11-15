5f7d43ce2d50cd09f350b5ae8ee3a0048e3dbf6d
Zvonko Kosic <zkosic@redhat.com>
2018-11-15 09:55:53 +0000

    Added controller

--- pkg/controller/nodefeaturediscovery/nodefeaturediscovery_controller.go
+++ pkg/controller/nodefeaturediscovery/nodefeaturediscovery_controller.go
@@ -51,7 +51,7 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 
 	// TODO(user): Modify this to be the types you create that are owned by the primary resource
 	// Watch for changes to secondary resource Pods and requeue the owner NodeFeatureDiscovery
-	err = c.Watch(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForOwner{
+	err = c.Watch(&source.Kind{Type: &appsv1.DaemonSet{}}, &handler.EnqueueRequestForOwner{
 		IsController: true,
 		OwnerType:    &nodefeaturediscoveryv1alpha1.NodeFeatureDiscovery{},
 	})
