7189cf948f55cdcc30f144a38839bb0f5d0bc8fb
Zvonko Kosic <zkosic@redhat.com>
2018-12-03 15:04:00 +0100

    Added ConfigMap

--- pkg/controller/nodefeaturediscovery/nodefeaturediscovery_controller.go
+++ pkg/controller/nodefeaturediscovery/nodefeaturediscovery_controller.go
@@ -4,8 +4,8 @@ import (
 	"context"
 
 	nodefeaturediscoveryv1alpha1 "github.com/openshift/cluster-nfd-operator/pkg/apis/nodefeaturediscovery/v1alpha1"
-	appsv1 "k8s.io/api/apps/v1"
-
+	//appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
