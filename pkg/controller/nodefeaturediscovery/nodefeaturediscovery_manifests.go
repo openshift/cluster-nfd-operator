bb22bde34d64c67a07fc1f834c928ac0bb6f9489
Zvonko Kosic <zkosic@redhat.com>
2018-12-20 13:39:33 +0100

    Added Manifests Reader

--- pkg/controller/nodefeaturediscovery/nodefeaturediscovery_manifests.go
+++ pkg/controller/nodefeaturediscovery/nodefeaturediscovery_manifests.go
@@ -21,11 +21,14 @@ func FilePathWalkDir(root string) ([]string, error) {
     return files, err
 }
 
-var nfdserviceaccount []byte
+var nfdserviceaccount            []byte
+var nfdclusterrole               []byte
+var nfdclusterrolebinding        []byte
+var nfdsecuritycontextconstraint []byte
+var nfdconfigmap                 []byte
+var nfddaemonset                 []byte
 
 func GenerateManifests() {
-	// Were putting all assets to /opt because this is one
-	// of the directories that is writable in RHCOS.
 	assets := "/opt/lib/cluster-nfd-operator/assets/node-feature-discovery"
 	files, err := FilePathWalkDir(assets)
 	if err != nil {
@@ -38,186 +41,13 @@ func GenerateManifests() {
 			panic(err)
 		}
 		manifests = append(manifests, buffer)
-		log.Printf("MANIFEST: %s %d", manifests[i])
 	}
-
-	nfdserviceaccount = manifests[0]
-	log.Printf("MANIFEST: %s ", manifests[0])
-	log.Printf("SERVICE: %s ", nfdserviceaccount)
-
+	
+	nfdserviceaccount            = manifests[0]
+	nfdclusterrole               = manifests[1]
+	nfdclusterrolebinding        = manifests[2]
+	nfdsecuritycontextconstraint = manifests[3]
+	nfdconfigmap                 = manifests[4]
+	nfddaemonset                 = manifests[5]
 }
 
-// var nfdserviceaccount = []byte(`
-// apiVersion: v1
-// kind: ServiceAccount
-// metadata:
-//   name: node-feature-discovery
-//   namespace: openshift-cluster-nfd-operator
-// `)
-
-//var nfdserviceaccount = manifests[0]
-
-var nfdclusterrole = []byte(`
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRole
-metadata:
-  name: node-feature-discovery
-rules:
-- apiGroups:
-  - ""
-  resources:
-  - pods
-  - nodes
-  verbs:
-  - get
-  - patch
-  - update
-- apiGroups:
-  - security.openshift.io
-  resources:
-  - securitycontextconstraints
-  verbs:
-  - use
-  resourceNames:
-  - privileged
-`)
-
-var nfdclusterrolebinding = []byte(`
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  name: node-feature-discovery
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: node-feature-discovery
-subjects:
-- kind: ServiceAccount
-  name: node-feature-discovery
-  namespace: openshift-cluster-nfd-operator
-userNames:
-- system:serviceaccount:openshift-cluster-nfd-operator:node-feature-discovery
-`)
-
-var nfdsecuritycontextconstraint = []byte(`
-allowHostDirVolumePlugin: true
-allowHostIPC: false
-allowHostNetwork: true
-allowHostPID: false
-allowHostPorts: false
-allowPrivilegeEscalation: true
-allowPrivilegedContainer: false
-allowedCapabilities: null
-apiVersion: security.openshift.io/v1
-defaultAddCapabilities: null
-fsGroup:
-  type: RunAsAny
-groups: []
-kind: SecurityContextConstraints
-metadata:
-  annotations:
-    kubernetes.io/description: 'hostmount-anyuid provides all the features of the
-      restricted SCC but allows host mounts and any UID by a pod.  This is primarily
-      used by the persistent volume recycler. WARNING: this SCC allows host file system
-      access as any UID, including UID 0.  Grant with caution.'
-  creationTimestamp: 2018-10-23T09:00:53Z
-  name: node-feature-discovery
-  resourceVersion: "156705"
-  selfLink: /apis/security.openshift.io/v1/securitycontextconstraints/node-feature-discovery
-  uid: 253d2129-d6a2-11e8-a7ea-06f9c2879cd2
-priority: null
-readOnlyRootFilesystem: false
-requiredDropCapabilities:
-- MKNOD
-runAsUser:
-  type: RunAsAny
-seLinuxContext:
-  type: MustRunAs
-supplementalGroups:
-  type: RunAsAny
-users:
-- system:serviceaccount:openshift-cluster-nfd-operator:node-feature-discovery
-volumes:
-- configMap
-- downwardAPI
-- emptyDir
-- hostPath
-- nfs
-- persistentVolumeClaim
-- projected
-- secret
-`)
-
-
-var nfdconfigmap = []byte(`
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: node-feature-discovery
-  namespace: openshift-cluster-nfd-operator
-data:
-  node-feature-discovery-conf: |
-    #uncomment to configure
-    #sources:
-    #  pci:
-    #    deviceClassWhitelist:
-    #      - "0200"
-    #      - "03"
-    #      - "12"
-    #    deviceLabelFields:
-    #      - "class"
-    #      - "vendor"
-    #      - "device"
-    #      - "subsystem_vendor"
-    #      - "subsystem_device"
-`)
-
-
-var nfddaemonset = []byte(`
-apiVersion: apps/v1
-kind: DaemonSet
-metadata:
-  labels:
-    app: node-feature-discovery
-  name: node-feature-discovery
-  namespace: openshift-cluster-nfd-operator
-spec:
-  selector:
-    matchLabels:
-      app: node-feature-discovery
-  template:
-    metadata:
-      labels:
-        app: node-feature-discovery
-    spec:
-      hostNetwork: true
-      serviceAccount: node-feature-discovery
-      containers:
-        - env:
-          - name: NODE_NAME
-            valueFrom:
-              fieldRef:
-                fieldPath: spec.nodeName
-          image: quay.io/zvonkok/node-feature-discovery:v0.3.0-10-g86947fc-dirty
-          name: node-feature-discovery
-          command: ["/usr/bin/node-feature-discovery", "--source=pci"]
-          args:
-            - "--sleep-interval=60s"
-          volumeMounts:
-            - name: host-sys
-              mountPath: "/host-sys"
-            - name: config
-              mountPath: "/etc/kubernetes/node-feature-discovery"
-          securityContext:
-            privileged: true
-      volumes:
-        - name: host-sys
-          hostPath:
-            path: "/sys"
-        - name: config
-          configMap:
-            name: node-feature-discovery
-            items:
-              - key: node-feature-discovery-conf
-                path: node-feature-discovery.conf
-`)
