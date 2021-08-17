a379536be5ed2cb6ceb0322b80081faa5e9d4d46
Courtney Pacheco <cpacheco@redhat.com>
2021-08-17 14:40:11 -0400

    Remove unused logger in 'main.go'
    Remove unused logger now that a NodeFeatureDiscoveryLogger struct
    has been created.

--- main.go
+++ main.go
@@ -126,7 +126,6 @@ func main() {
 
 	if err = (&controllers.NodeFeatureDiscoveryReconciler{
 		Client: mgr.GetClient(),
-		Log:    ctrl.Log.WithName("controllers").WithName("NodeFeatureDiscovery"),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "NodeFeatureDiscovery")
