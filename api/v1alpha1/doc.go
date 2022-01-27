0383dfdb7bd0738ee321ef80fe68fa481cf8b710
Carlos Eduardo Arango Gutierrez <carangog@redhat.com>
2022-01-26 21:09:01 -0500

    NodeFeatureRules add new provided API
    Signed-off-by: Carlos Eduardo Arango Gutierrez <carangog@redhat.com>

--- api/v1alpha1/doc.go
+++ api/v1alpha1/doc.go
@@ -1,4 +1,20 @@
-// Package v1alpha1 contains API Schema definitions for the nfd v1alpha1 API group
-// +k8s:deepcopy-gen=package,register
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package v1alpha1 is the v1alpha1 version of the nfd API.
+// +k8s:deepcopy-gen=package
+// +kubebuilder:object:generate=true
 // +groupName=nfd.openshift.io
 package v1alpha1
--- go.mod
+++ go.mod
@@ -3,16 +3,16 @@ module github.com/openshift/cluster-nfd-operator
 go 1.16
 
 require (
-	github.com/onsi/ginkgo v1.14.1
-	github.com/onsi/gomega v1.10.2
+	github.com/onsi/ginkgo v1.16.4
+	github.com/onsi/gomega v1.15.0
 	github.com/openshift/api v3.9.0+incompatible
 	github.com/openshift/client-go v3.9.0+incompatible
 	github.com/openshift/custom-resource-status v0.0.0-20210221154447-420d9ecf2a00
-	github.com/prometheus/client_golang v1.8.0
-	k8s.io/api v0.20.4
-	k8s.io/apimachinery v0.20.4
-	k8s.io/client-go v0.20.4
+	github.com/prometheus/client_golang v1.11.0
+	k8s.io/api v0.22.2
+	k8s.io/apimachinery v0.22.2
+	k8s.io/client-go v0.22.2
 	k8s.io/klog/v2 v2.9.0
-	k8s.io/kubectl v0.20.4
-	sigs.k8s.io/controller-runtime v0.7.0
+	k8s.io/kubectl v0.22.2
+	sigs.k8s.io/controller-runtime v0.10.2
 )
