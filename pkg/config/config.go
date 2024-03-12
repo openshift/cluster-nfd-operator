585d3fd1e13b2eb164c7a8ed871cbbfc2e99548c
Chris Procter <cprocter@redhat.com>
2024-03-12 09:51:17 +0000

    replace old version numbers with the correct 4.16

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -23,7 +23,7 @@ import (
 )
 
 const (
-	nodeFeatureDiscoveryImageDefault string = "quay.io/openshift/origin-node-feature-discovery:4.15"
+	nodeFeatureDiscoveryImageDefault string = "quay.io/openshift/origin-node-feature-discovery:4.16"
 	contextTimeout                          = 300 * time.Second
 	// A number in seconds to define a context Timeout
 	// E.g. if 5 seconds is wanted, the CTX_TIMEOUT=5
