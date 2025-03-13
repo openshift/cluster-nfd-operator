1e51bce237c7171bc703f81706174a1f014b873a
Yevgeny Shnaidman <yshnaidm@redhat.com>
2025-03-13 20:03:03 +0200

    Adding Tolerations fields to the NFD CRD
    Currently, while deploying operand only, user can specify tolerations
    for both master depoyment and worker daemonsets. We want to add this
    functionlity to operator deployment also, using dedicated fields in the
    CRD

--- internal/client/client.go
+++ internal/client/client.go
@@ -15,5 +15,7 @@ limitations under the License.
 */
 
 package client
+
 import _ "go.uber.org/mock/mockgen/model"
+
 //go:generate mockgen -package=client -destination mock_client.go sigs.k8s.io/controller-runtime/pkg/client Client,StatusWriter
