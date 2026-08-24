9ced6ef553475ba9bdcf3bf18bc89ba2b4df22bc
Tomer Newman <tnewman@redhat.com>
2026-08-24 13:55:17 +0300

    Revert "[release-4.22] OCPBUGS-105462:Surface OperandImagePinned condition and warning event"

--- main.go
+++ main.go
@@ -155,8 +155,6 @@ func main() {
 	sccAPI := scc.NewSccAPI(client, scheme)
 	statusAPI := status.NewStatusAPI(deploymentAPI, daemonsetAPI)
 
-	recorder := mgr.GetEventRecorderFor("nodefeaturediscovery-controller")
-
 	if err = new_controllers.NewNodeFeatureDiscoveryReconciler(client,
 		deploymentAPI,
 		daemonsetAPI,
@@ -164,8 +162,7 @@ func main() {
 		jobAPI,
 		sccAPI,
 		statusAPI,
-		scheme,
-		recorder).SetupWithManager(mgr); err != nil {
+		scheme).SetupWithManager(mgr); err != nil {
 		setupLogger.Error(err, "unable to create controller", "controller", "NodeFeatureDiscovery")
 		os.Exit(1)
 	}
